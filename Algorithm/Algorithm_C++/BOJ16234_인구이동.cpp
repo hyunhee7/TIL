48a465048963d03b733de1d153437538499d66ac
hyunhee7 <hyunhi7@naver.com>
2020-01-31 12:12:22 +0900

    BOJ16234 added

--- Algorithm/Algorithm_C++/BOJ16234_인구이동.cpp
+++ Algorithm/Algorithm_C++/BOJ16234_인구이동.cpp
@@ -1,62 +1,65 @@
 /*
-	17:30-19:00
+	11:00-12:30
 */
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 int n, l, r;
 int map[51][51];
 bool visited[51][51];
-int answer = 0;
 int dx[] = { 1,-1,0,0 };
 int dy[] = { 0,0,1,-1 };
-bool bfs(int a, int b) {
-	queue<pair<int,int>> q;
+int answer = 0; bool isMove1;
+void bfs(int a, int b) {
+	queue<pair<int, int>> q;
 	q.push(make_pair(a, b));
-	int total = 0;
-	int num = 0;
-	bool isVisit = false;
-	for (int i = 0; i < n; i++) {
-		fill(visited[i], visited[i] + 51, false);
-	}
+	bool isMove = false;
+	vector<pair<int, int>> contry;
+	int total = map[a][b];
+	int num = 1;
+	visited[a][b] = true;
 	while (!q.empty()) {
 		int x = q.front().first;
 		int y = q.front().second;
+		contry.push_back(make_pair(x, y));
 		q.pop();
-		visited[x][y] = true;
-		total += map[x][y];
-		num++;
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
 			if (visited[nx][ny]) continue;
-			if (abs(map[nx][ny] - map[x][y]) >= l && abs(map[nx][ny] - map[x][y]) <= r) {
+			if (abs(map[x][y] - map[nx][ny]) >= l && abs(map[x][y] - map[nx][ny]) <= r) {
+				visited[nx][ny] = true;
+				total += map[nx][ny];
+				num++;
+				isMove = true;
 				q.push(make_pair(nx, ny));
-				isVisit = true;
 			}
 		}
 	}
-	if (num <= 1) return true;
-	if (!isVisit) return true;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (visited[i][j]) {
-				map[i][j] = (total / num);
-			}
-			cout << visited[i][j] << ' ';
-		}
-		cout << endl;
+	if (!isMove) {
+		visited[a][b] = false;
+		isMove1 = false;
+		return;
 	}
-	cout << "---------------" << endl;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << map[i][j] << ' ';
+	else {
+
+		//cout << total << " " << contry.size() << endl;
+		for (int i = 0; i < contry.size(); i++) {
+			//cout << i << endl;
+			map[contry[i].first][contry[i].second] = (total / contry.size());
 		}
-		cout << endl;
+		isMove1 = true;
+		//for (int i = 0; i < n; i++) {
+		//	for (int j = 0; j < n; j++) {
+		//		cout << map[i][j] << ' ';
+		//	}
+		//	cout << endl;
+		//}
+		//cout << "--------------" <<endl;
+		return;
 	}
-	cout << "---------------" << endl;
-	return false;
 }
 int main() {
 	cin >> n >> l >> r;
@@ -67,12 +70,25 @@ int main() {
 	}
 	bool isFin = false;
 	while (!isFin) {
-		//인구이동
+		for (int i = 0; i < n; i++) {
+			fill(visited[i], visited[i] + 51, false);
+		}
+		bool checking = false;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				isFin = bfs(i, j);
-				if (!isFin) answer++;
+				if (!visited[i][j]) {
+					bfs(i, j);
+					if (isMove1) {
+						checking = true;
+					}
+				}
 			}
+		}
+		if (checking) {
+			answer++;
+		}
+		else {
+			isFin = true;
 		}
 	}
 	cout << answer;
