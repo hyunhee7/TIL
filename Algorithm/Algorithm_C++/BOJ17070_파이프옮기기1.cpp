caf211580d1a6020904c82ff137509e6386b8f5b
hyunhee7 <hyunhi7@naver.com>
2020-03-03 11:52:13 +0900

    BOJ17070 solved

--- Algorithm/Algorithm_C++/BOJ17070_파이프옮기기1.cpp
+++ Algorithm/Algorithm_C++/BOJ17070_파이프옮기기1.cpp
@@ -7,18 +7,36 @@ bool visited[17][17];
 int dx[] = { 0,1,1 };
 int dy[] = { 1,1,0 }; //가로, 대각, 세로
 int answer = 0;
-void dfs(int x, int y, int dir) {
+void dfs(int bx, int by, int x, int y, int dir) {
 	if (x == n-1 && y == n-1) {
 		answer++;
 		return;
 	}
+	visited[bx][by] = 0;
 	//현재그대로
 	int nx = x + dx[dir];
 	int ny = y + dy[dir];
 	if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
-		if (map[nx][ny] == 0 && !visited[nx][ny]) {
-			dfs(nx, ny, dir);
+		if (dir != 1) {
+			if (map[nx][ny] == 0 && !visited[nx][ny]) {
+				visited[nx][ny] = 1;
+				dfs(x, y, nx, ny, dir);
+				visited[nx][ny] = 0;
+			}
 		}
+		else {
+			if (map[nx][ny] == 0 && map[nx - 1][ny] == 0 && map[nx][ny - 1] == 0 &&
+				!visited[nx][ny] && !visited[nx - 1][ny] && !visited[nx][ny - 1]) {
+				visited[nx][ny] = 1;
+				visited[nx - 1][ny] = 1;
+				visited[nx][ny - 1] = 1;
+				dfs(x, y, nx, ny, dir);
+				visited[nx][ny] = 0;
+				visited[nx - 1][ny] = 0;
+				visited[nx][ny - 1] = 0;
+			}
+		}
+		
 	}
 	//가로, 세로
 	if (dir == 1) {
@@ -27,7 +45,7 @@ void dfs(int x, int y, int dir) {
 		if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
 			if (map[nx][ny] == 0 && !visited[nx][ny]) {
 				visited[nx][ny] = 1;
-				dfs(nx, ny, dir - 1);
+				dfs(x, y, nx, ny, dir - 1);
 				visited[nx][ny] = 0;
 			}
 		}
@@ -36,7 +54,7 @@ void dfs(int x, int y, int dir) {
 		if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
 			if (map[nx][ny] == 0 && !visited[nx][ny]) {
 				visited[nx][ny] = 1;
-				dfs(nx, ny, dir + 1);
+				dfs(x, y, nx, ny, dir + 1);
 				visited[nx][ny] = 0;
 			}
 		}
@@ -46,11 +64,12 @@ void dfs(int x, int y, int dir) {
 		nx = x + dx[1];
 		ny = y + dy[1];
 		if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
-			if (map[nx][ny] == 0 && !visited[nx][ny]) {
+			if (map[nx][ny] == 0 && map[nx-1][ny] == 0 && map[nx][ny-1] == 0 &&
+				!visited[nx][ny] && !visited[nx-1][ny] && !visited[nx][ny-1]) {
 				visited[nx][ny] = 1;
 				visited[nx - 1][ny] = 1;
 				visited[nx][ny - 1] = 1;
-				dfs(nx, ny, 1);
+				dfs(x, y, nx, ny, 1);
 				visited[nx][ny] = 0;
 				visited[nx - 1][ny] = 0;
 				visited[nx][ny - 1] = 0;
@@ -65,6 +84,8 @@ int main() {
 			cin >> map[i][j];
 		}
 	}
-	dfs(0, 1, 0);
+	visited[0][0] = 1;
+	visited[0][1] = 1;
+	dfs(0, 0, 0, 1, 0);
 	cout << answer;
 }
