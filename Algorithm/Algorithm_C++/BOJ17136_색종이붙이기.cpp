97eaffb59f69bcaebb1452e82266a6bf00fa1390
hyunhee7 <hyunhi7@naver.com>
2020-03-25 11:04:22 +0900

    BOJ17136 solved

--- Algorithm/Algorithm_C++/BOJ17136_색종이붙이기.cpp
+++ Algorithm/Algorithm_C++/BOJ17136_색종이붙이기.cpp
@@ -1,117 +1,63 @@
-/*
-	20:00 - 21:30
-*/
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 int map[10][10];
-bool visited[10][10];
-vector<pair<int, int>> info;
-int val = 0;
-bool onef(int x, int y) {
-	if (!visited[x][y]) {
-		visited[x][y] = true;
-		return true;
-	}
-	return false;
-}
-bool twof(int x, int y) {
-	if (!visited[x][y] && !visited[x][y + 1] && map[x][y + 1] == 1
-		&& !visited[x + 1][y] && map[x + 1][y] == 1 &&
-		!visited[x + 1][y + 1] && map[x + 1][y + 1] == 1) {
-		return true;
-	}
-	return false;
-}
-bool threef(int x, int y) {
-	if (!visited[x][y] 
-		&& !visited[x][y + 1] && map[x][y + 1] == 1
-		&& !visited[x][y + 2] && map[x][y + 2] == 1
-		&& !visited[x + 1][y] && map[x + 1][y] == 1
-		&& !visited[x + 2][y] && map[x + 2][y] == 1
-		&& !visited[x + 1][y + 1] && map[x + 1][y + 1] == 1
-		&& !visited[x + 2][y + 2] && map[x + 2][y + 2] == 1) {
-		return true;
-	}
-	return false;
-}
-bool fourf(int x, int y) {
-	if (!visited[x][y]
-		&& !visited[x][y + 1] && map[x][y + 1] == 1
-		&& !visited[x][y + 2] && map[x][y + 2] == 1
-		&& !visited[x][y + 3] && map[x][y + 3] == 1
-		&& !visited[x + 1][y] && map[x + 1][y] == 1
-		&& !visited[x + 2][y] && map[x + 2][y] == 1
-		&& !visited[x + 3][y] && map[x + 3][y] == 1
-		&& !visited[x + 1][y + 1] && map[x + 1][y + 1] == 1
-		&& !visited[x + 2][y + 2] && map[x + 2][y + 2] == 1
-		&& !visited[x + 3][y + 3] && map[x + 3][y + 3] == 1) {
-		return true;
-	}
-	return false;
-}
-bool fivef(int x, int y) {
-	if (!visited[x][y]
-		&& !visited[x][y + 1] && map[x][y + 1] == 1
-		&& !visited[x][y + 2] && map[x][y + 2] == 1
-		&& !visited[x][y + 3] && map[x][y + 3] == 1
-		&& !visited[x][y + 4] && map[x][y + 4] == 1
-		&& !visited[x + 1][y] && map[x + 1][y] == 1
-		&& !visited[x + 2][y] && map[x + 2][y] == 1
-		&& !visited[x + 3][y] && map[x + 3][y] == 1
-		&& !visited[x + 4][y] && map[x + 4][y] == 1
-		&& !visited[x + 1][y + 1] && map[x + 1][y + 1] == 1
-		&& !visited[x + 2][y + 2] && map[x + 2][y + 2] == 1
-		&& !visited[x + 3][y + 3] && map[x + 3][y + 3] == 1
-		&& !visited[x + 4][y + 4] && map[x + 4][y + 4] == 1) {
-		return true;
-	}
-	return false;
-}
-void dfs(int one, int two, int three, int four, int five) {
-	if (one == 5 && two == 5 && three == 5 && four == 5 && five == 5) {
-		
+int answer = 987654321;
+int paper[6] = { 0,5,5,5,5,5 };
+int cnt;
+void dfs(int x, int y) {
+	if (y == 10) {
+		dfs(x + 1, 0);
 		return;
 	}
-	for (int i = 0; i < info.size(); i++) {
-		if (!visited[info[i].first][info[i].second]) {
-			bool tmp1 = onef(info[i].first,info[i].second);
-			if (tmp1) {
-				dfs(one + 1, two, three, four, five);
+	if (x == 10) {
+		answer = min(answer, cnt);
+		return;
+	}
+	if (map[x][y] == 0) {
+		dfs(x, y + 1);
+		return;
+	}
+	for (int i = 5; i >= 1; i--) {
+		if (paper[i] == 0 || x + i > 10 || y + i > 10)continue;
+		bool flag = true;
+		for (int j = 0; j < i; j++) {
+			for (int k = 0; k < i; k++) {
+				if (map[x + j][y + k] == 0) {
+					flag = false;
+					break;
+				}
 			}
-			else return;
-			bool tmp2 = twof(info[i].first, info[i].second);
-			if (tmp2) {
-				dfs(one, two + 1, three, four, five);
-			}
-			else return;
-			bool tmp3 = threef(info[i].first, info[i].second);
-			if (tmp3) {
-				dfs(one, two, three + 1, four, five);
-			}
-			else return;
-			bool tmp4 = fourf(info[i].first, info[i].second);
-			if (tmp4) {
-				dfs(one, two, three, four + 1, five);
-			}
-			else return;
-			bool tmp5 = fivef(info[i].first, info[i].second);
-			if (tmp5) {
-				dfs(one, two, three, four, five + 1);
-			}
-			else return;
-			return;
+			if (!flag) break;
 		}
+		if (!flag) continue;
+		for (int j = 0; j < i; j++) {
+			for (int k = 0; k < i; k++) {
+				map[x + j][y + k] = 0;
+			}
+		}
+		paper[i]--;
+		cnt++;
+		dfs(x, y + i);
+		for (int j = 0; j < i; j++) {
+			for (int k = 0; k < i; k++) {
+				map[x + j][y + k] = 1;
+			}
+		}
+		paper[i]++;
+		cnt--;
 	}
 }
 int main() {
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
 			cin >> map[i][j];
-			if (map[i][j] == 1) {
-				info.push_back({ i,j });
-			}
 		}
 	}
-	dfs(0, 0, 0, 0, 0);
+	dfs(0, 0);
+	if (answer == 987654321) {
+		cout << -1 << endl;
+	}
+	else
+		cout << answer << endl;
 }
