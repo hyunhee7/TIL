502b77dcb1fae6dacd510e708678674c900ee015
hyunhee7 <hyunhi7@naver.com>
2020-04-03 18:58:54 +0900

    BOJ17472 ing

--- Algorithm/Algorithm_C++/BOJ17471_게리맨더링_2.cpp
+++ Algorithm/Algorithm_C++/BOJ17471_게리맨더링_2.cpp
@@ -1,22 +1,58 @@
+#define MAX 987654321
 #include<iostream>
+#include <bitset>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int n;
+vector<int> adj[11];
+int n,m,temp;
 int a[11];
 int map[11][11];
 int c[11];
+int comp[11], visited[11];
+int answer = MAX;
+pair<int, int> dfs(int now, int value) {
+	visited[now] = true;
+	pair<int, int> ret = { 1,a[now] };
+	for (int t : adj[now]) {
+		if (comp[t] != value) continue;
+		if (visited[t])continue;
+		pair<int, int> _temp = dfs(t, value);
+		ret.first += _temp.first;
+		ret.second += _temp.second;
+	}
+	return ret;
+}
 int main() {
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
-	for (int i = 1,t; i <= n; i++) {
-		cin >> t;
-		for (int j = 0, tmp; j < t; j++) {
-			cin >> tmp;
-			map[i][tmp] = 1;
+	for (int i = 1; i <= n; i++) {
+		cin >> m;
+		for (int j = 0; j < m; j++) {
+			cin >> temp;
+			adj[i].push_back(temp);
+			adj[temp].push_back(i);
 		}
 	}
-	/*
-		bitmask써보기
-	*/
+	for (int i = 1; i < (1 << n) - 1; i++) { // 노드를 돈다.
+		fill(comp, comp + 11, 0);
+		fill(visited, visited + 11, 0);
+		int idx1 = -1, idx2 = -1;
+		for (int j = 0; j < n; j++) {		// 간선 존재 체크
+			if (i & (1 << j)) {
+				comp[j + 1] = 1;			//존재하면 idx1에 인덱스 넣기
+				idx1 = j + 1;
+			}
+			else idx2 = j + 1;				//없으면 idx2에 넣기
+		}
+		pair<int, int> comp1 = dfs(idx1, 1); //idx1
+		pair<int, int> comp2 = dfs(idx2, 0); //idx2
+		if (comp1.first + comp2.first == n) answer = min(answer, abs(comp1.second - comp2.second));
+	}
+	if (answer == MAX) {
+		answer = -1;
+	}
+	cout << answer << endl;
 }
---
+++ Algorithm/Algorithm_C++/BOJ17472_다리만들기2.cpp
@@ -0,0 +1,172 @@
+/*
+	14:45 - 16:15
+*/
+/*
+예외case(https://www.acmicpc.net/board/view/41802)
+ans:13
+
+10 6
+0 0 0 1 0 0
+0 0 0 1 0 0
+0 1 0 0 0 1
+0 0 0 0 0 0
+1 1 0 1 1 0
+1 0 0 0 1 0
+1 1 0 0 1 0
+0 0 0 0 1 1
+0 0 0 0 0 0
+0 1 0 0 0 0
+*/
+#define MAX 987654321
+#include <iostream>
+#include <queue>
+using namespace std;
+int answer = MAX;
+int n, m;
+int map[11][11];
+bool visited[11][11];
+int dx[] = { 1,-1,0,0 };
+int dy[] = { 0,0,1,-1 };
+int dist[7][7];
+int dvisited[7];
+void bfs(int a, int b, int val) {
+	queue<pair<int, int>> q;
+	q.push({ a,b });
+	map[a][b] = val;
+	visited[a][b] = true;
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+			if (!visited[nx][ny] && map[nx][ny]) {
+				visited[nx][ny] = true;
+				map[nx][ny] = val;
+				q.push({ nx,ny });
+
+			}
+		}
+	}
+}
+void dfs(int index, int cnt, int dnum, int res) {
+	if (cnt == dnum - 1) {
+		//cout << cnt << endl;
+		answer = min(res, answer);
+		for (int i = 1; i < dnum; i++) {
+			cout << dvisited[i] << ' ';
+		}
+		cout << endl;
+		return;
+	}
+	//if (res > answer) return;
+	//if (cnt > dnum)return;
+	dvisited[index] = true;
+
+	for (int i = 1; i < dnum; i++) {
+		
+		if (i == index) continue;
+		if (dist[index][i] > 0 && !dvisited[i]) {
+			
+			dvisited[i] = true;
+			cout << "indx:" << index << ",i :"<<i << ", dst:" << dist[index][i] <<", res:"<<res+dist[index][i] << endl;
+			//cout << i << endl;
+			cout << "cnt:"<<cnt << endl;
+			dfs(index, cnt + 1, dnum, res + dist[index][i]);
+			dfs(i, cnt + 1, dnum, res + dist[index][i]);
+			dvisited[i] = false;
+		}
+	}
+	//dvisited[index] = false;
+}
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+		}
+	}
+	//땅번호 표시
+	int d[7];
+	int dnum = 1;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (map[i][j]==1 && !visited[i][j]) {
+				bfs(i, j, dnum);
+				dnum++;
+			}
+			
+		}
+	}
+	cout << "------" << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << map[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	//다리놓기
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (map[i][j] > 0 && j+1<m && map[i][j+1]==0) {//가로 다리놓기
+				for (int k = j + 1; k < m; k++) {
+					if (map[i][k] > 0) {
+						int idx1 = map[i][j];
+						int idx2 = map[i][k];
+						int val = k - j - 1;
+						if (val <= 1) break;
+						//cout << idx1 << " " << idx2 << " " << val << endl;
+						//cout << "i,j :" << i << ", " << j << ", [k,j] : " << k << " " << j << endl;
+						if (dist[idx1][idx2] > 0) {
+							dist[idx1][idx2] = min(val, dist[idx1][idx2]);
+							dist[idx2][idx1] = min(val, dist[idx2][idx1]);
+						}
+						else {
+							dist[idx1][idx2] = val;
+							dist[idx2][idx1] = val;
+						}
+						break;
+					}
+				}
+			}
+			if (map[i][j] > 0 && i+1<n && map[i + 1][j] == 0) {//세로 다리놓기
+				for (int k = i + 1; k < n; k++) {
+					if (map[k][j] > 0) {
+						//cout << "dddd:" <<map[k][j] << endl;
+						int idx1 = map[i][j];
+						int idx2 = map[k][j];
+						//cout << "i:" << i << ", k:" << k <<",j:"<<j << endl;
+						int val = k - i - 1;
+						//cout << "val:" << val<<endl;
+						if (val <= 1)break;
+						//cout << idx1 << " " << idx2 << " " << val << endl;
+						//cout << "i,j :" << i << ", " << j << ", [k,j] : " << k << " " << j << endl;
+						if (dist[idx1][idx2] > 0) {
+							dist[idx1][idx2] = min(val, dist[idx1][idx2]);
+							dist[idx2][idx1] = min(val, dist[idx2][idx1]);
+						}
+						else {
+							dist[idx1][idx2] = val;
+							dist[idx2][idx1] = val;
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+	//최단거리 구하기
+	//dfs(1,1,dnum, 0);
+	dfs(1, 1, dnum, 0);
+	cout << "------" << endl;
+	for (int i = 1; i <= 6; i++) {
+		for (int j = 1; j <= 6; j++) {
+			cout << dist[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	if (answer == MAX) answer = -1;
+	cout << answer << endl;
+}
